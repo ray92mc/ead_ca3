f520557dcd7a327458acfcfc3b1badb2b2faee68
Raymond McCarthy <x00179223@mytudublin.ie>
2022-12-09 13:17:01 +0000

    fix code smell

--- SeleniumTests/NavigatePagesTest.cs
+++ SeleniumTests/NavigatePagesTest.cs
@@ -3,43 +3,52 @@ using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
 
-[TestFixture]
-public class NavigatePagesTest {
- 
-  [Test]
-  public void navigatePages() {
 
-        var options = new ChromeOptions();
-        options.AddArgument("--headless");
-        using IWebDriver driver = new ChromeDriver(options);
+namespace EADCA3
+{
+    [TestFixture]
+    public class NavigatePagesTest
+    {
 
+        [Test]
+        public void navigatePages()
+        {
 
-        driver.Navigate().GoToUrl("https://localhost:44370/");
-
-        WebDriverWait _wait = new WebDriverWait(driver, new TimeSpan(0, 1, 0));
-        _wait.Until(d => d.FindElement(By.Id("pageIndexText")));
-
-        Assert.That(driver.FindElement(By.Id("pageIndexText")).Text, Is.EqualTo("1 / 500"));
+            var options = new ChromeOptions();
+            options.AddArgument("--headless");
+            using IWebDriver driver = new ChromeDriver(options);
 
 
-        _wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.Id("nextPageButton"))));
-        driver.FindElement(By.Id("nextPageButton")).Click();
+            driver.Navigate().GoToUrl("https://localhost:44370/");
 
-        Assert.That(driver.FindElement(By.Id("pageIndexText")).Text, Is.EqualTo("2 / 500"));
+            WebDriverWait _wait = new WebDriverWait(driver, new TimeSpan(0, 1, 0));
+            _wait.Until(d => d.FindElement(By.Id("pageIndexText")));
 
-        _wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.Id("prevPageButton"))));
-        driver.FindElement(By.Id("prevPageButton")).Click();
+            Assert.That(driver.FindElement(By.Id("pageIndexText")).Text, Is.EqualTo("1 / 500"));
 
-        Assert.That(driver.FindElement(By.Id("pageIndexText")).Text, Is.EqualTo("1 / 500"));
 
-        driver.FindElement(By.Id("selectPageInputBox")).Click();
+            _wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.Id("nextPageButton"))));
+            driver.FindElement(By.Id("nextPageButton")).Click();
 
-        driver.FindElement(By.Id("selectPageInputBox")).SendKeys("50");
+            Assert.That(driver.FindElement(By.Id("pageIndexText")).Text, Is.EqualTo("2 / 500"));
 
-        _wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.Id("selectPageButton"))));
-        driver.FindElement(By.Id("selectPageButton")).Click();
+            _wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.Id("prevPageButton"))));
+            driver.FindElement(By.Id("prevPageButton")).Click();
 
-        Assert.That(driver.FindElement(By.Id("pageIndexText")).Text, Is.EqualTo("50 / 500"));
-        
-  }
+            Assert.That(driver.FindElement(By.Id("pageIndexText")).Text, Is.EqualTo("1 / 500"));
+
+            driver.FindElement(By.Id("selectPageInputBox")).Click();
+
+            driver.FindElement(By.Id("selectPageInputBox")).SendKeys("50");
+
+            _wait.Until(ExpectedConditions.ElementToBeClickable(driver.FindElement(By.Id("selectPageButton"))));
+            driver.FindElement(By.Id("selectPageButton")).Click();
+
+            Assert.That(driver.FindElement(By.Id("pageIndexText")).Text, Is.EqualTo("50 / 500"));
+
+        }
+    }
+
 }
+
+
